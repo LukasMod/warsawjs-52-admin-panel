e0285d60bc07607aae31464aef3b8d0d0cda9f0a
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-02-21 12:16:47 +0100

    add MockServiceWorker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import LoginPage from './pages/login';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
